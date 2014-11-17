3b4a8866e8edc303d37c9125b6c4b66df36170e0
Dimas Nuh Putra <dimas@antikode.com>
2014-11-17 16:38:38 +0700

    Commit

--- rkfc/index.php
+++ rkfc/index.php
@@ -75,17 +75,16 @@ include('static/body_loader.php');
         <div class="container">
           <div class="row">
             <div class="footer-col col-sm-9 col-sm-12 about">
-              <a href="http://www.ritra.com"><img src="<?php echo $prefix_url;?>assets/img/logo_footer-ritralogistics.png" width="200" class="pull-left hidden-xs hidden-sm" style="margin-top: 20px; margin-bottom: 60px"></a>
+              <a href="http://www.ritra.com"><img src="<?php echo $prefix_url;?>assets/img/logo-saka_white.png" width="200" class="pull-left hidden-xs hidden-sm" style="margin-top: 20px; margin-bottom: 60px"></a>
               <div class="footer-col-inner">
                 <h3 class="title">About Us</h3>
                 <p>Ritra Cargo offers tailored solutions to your freight forwarding needs, from sea and air shipments to customized logistics project. Ritra Cargo is a wholly owned subsidiary of Ritra Logistics.</p>
-                <p><a class="more" href="<?php echo $prefix_url;?>about">Learn more <i class="fa fa-long-arrow-right"></i></a></p>
-              </div><!--//footer-col-inner-->
+                </div><!--//footer-col-inner-->
             </div><!--//foooter-col--> 
             
             <div class="footer-col col-sm-3 col-sm-12 contact">
               <div class="footer-col-inner">
-                <h3 class="title">Contact Ritra</h3>
+                <h3 class="title">Contact Saka Trans</h3>
                 <div class="row">
                   <p class="tel col-sm-12"><i class="fa fa-phone"></i><?php echo $info['telephone'];?></p>
                   <p class="email col-sm-12"><i class="fa fa-envelope"></i><a href="mailto:<?php echo $info['email'];?>"><?php echo $info['email_display'];?></a></p>  
