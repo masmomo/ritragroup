604aa571e99ce90ba3f9a2237a0cb2cee27bab45
Dimas Nuh Putra <dimas@antikode.com>
2014-12-01 16:45:07 +0700

    Commit

--- rkfc/admin/custom/static/general.php
+++ rkfc/admin/custom/static/general.php
@@ -7,7 +7,7 @@
 
 /* --- DATABASE VARIABLE --- */
 define('DB_HOST', 'localhost');       // DATABASE HOST
-define('DB_NAME', 'anti_ritra');       // DATABASE NAME
+define('DB_NAME', 'anti_rkfc');       // DATABASE NAME
 define('DB_USER', 'root');  // DATABASE USERNAME
 define('DB_PASS', '');   // DATABASE PASSWORD
 
